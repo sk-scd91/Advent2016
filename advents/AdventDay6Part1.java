24e9dd882f8a619a8755bd9eb568cd6d4bda4803
sk-scd91 <scdeneen@gmail.com>
2016-12-06 00:52:00 -0500

    Refactored Day 6 Part 1 for inheritance.

--- advents/AdventDay6Part1.java
+++ advents/AdventDay6Part1.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by Sean Deneen on 12/6/16.
+ * Find the message using a repetition code.
  */
 public class AdventDay6Part1 implements Advent {
     @Override
@@ -19,10 +21,14 @@ public class AdventDay6Part1 implements Advent {
                 .mapToObj(i -> lines.stream().map(s -> s.substring(i, i + 1)))
                 .map(stringStream -> stringStream.collect(Collectors.groupingBy(i -> i, Collectors.counting())))
                 .map(map -> map.entrySet().stream())
-                .map(entryStream -> entryStream.sorted(Map.Entry.<String, Long>comparingByValue().reversed()))
+                .map(this::sortFrequencies)
                 .map(entryStream -> entryStream.map(Map.Entry::getKey).findFirst().orElse("-"))
                 .collect(Collectors.joining());
 
         return "The message is " + message;
+    }
+
+    protected Stream<Map.Entry<String, Long>> sortFrequencies(Stream<Map.Entry<String, Long>> frequencyStream) {
+        return frequencyStream.sorted(Map.Entry.<String, Long>comparingByValue().reversed());
     }
 }
