a7d0f45f5374eda87b65444c189929e60a64b6f5
sk-scd91 <scdeneen@gmail.com>
2016-12-17 01:25:04 -0500

    Completed the Solution to Day 17 Part 1.

---
+++ advents/AdventDay17Part1.java
@@ -0,0 +1,82 @@
+package com.skscd91.advent2016.advents;
+
+import java.io.BufferedReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.function.BooleanSupplier;
+
+/**
+ * Created by Sean Deneen on 12/17/16.
+ * Using an MD5 password, find the shortest path to the vault.
+ */
+public class AdventDay17Part1 implements Advent {
+
+    private static final String PASSCODE = "pgflpeqp";
+    private static final int GRID_WIDTH = 4;
+
+    private Deque<BooleanSupplier> searches;
+    private String minPath;
+
+    private MessageDigest initDigest() {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {}
+        return digest;
+    }
+
+    protected MessageDigest cloneDigest(MessageDigest digest) {
+        try {
+            return (MessageDigest) digest.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+
+    private boolean isOpen(int hex) {
+        return (hex & 0xf) > 0xa;
+    }
+
+    private MessageDigest nextDirection(MessageDigest digest, char dir) {
+        MessageDigest out = cloneDigest(digest);
+        out.update((byte)dir);
+        return out;
+    }
+
+    private boolean nextStep(MessageDigest digest, String path, int x, int y) {
+        if (x * y == (GRID_WIDTH - 1) * (GRID_WIDTH - 1)) {
+            minPath = path;
+            return true;
+        }
+        byte[] hash = cloneDigest(digest).digest();
+
+        if (y > 0 && isOpen(hash[0] >> 4))
+            searches.add(() -> nextStep(nextDirection(digest, 'U'), path + 'U', x, y - 1));
+        if (y + 1 < GRID_WIDTH && isOpen(hash[0]))
+            searches.add(() -> nextStep(nextDirection(digest, 'D'), path + 'D', x, y + 1));
+        if (x > 0 && isOpen(hash[1] >> 4))
+            searches.add(() -> nextStep(nextDirection(digest, 'L'), path + 'L', x - 1, y));
+        if (x + 1 < GRID_WIDTH && isOpen(hash[1]))
+            searches.add(() -> nextStep(nextDirection(digest, 'R'), path + 'R', x + 1, y));
+
+        return false;
+    }
+
+    @Override
+    public String compute(BufferedReader input) {
+        MessageDigest initialDigest = initDigest();
+        initialDigest.reset();
+        initialDigest.update(PASSCODE.getBytes());
+        minPath = "";
+        searches = new ArrayDeque<>();
+
+        searches.add(() -> nextStep(initialDigest, "", 0, 0));
+
+        while (!searches.isEmpty() && !searches.removeFirst().getAsBoolean())
+            ;
+
+        return "The minimum path is " + minPath;
+    }
+}
