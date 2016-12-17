01b7e045b7734b2a331117e9d9fbef42a27e9d11
sk-scd91 <scdeneen@gmail.com>
2016-12-16 20:24:55 -0500

    Completed the Solution to Day 16 Part 1.

---
+++ advents/AdventDay16Part1.java
@@ -0,0 +1,49 @@
+package com.skscd91.advent2016.advents;
+
+import java.io.BufferedReader;
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
+/**
+ * Created by Sean Deneen on 12/16/16.
+ * Fill up a 272 bit disk, then calculate its checksum.
+ */
+public class AdventDay16Part1 implements Advent {
+
+    private static final int SEED = 0b10001110011110000;
+    private static final int LIMIT = 272;
+
+    private BigInteger generateDragonCurve(BigInteger input) {
+        BigInteger a = input.shiftLeft(input.bitLength());
+        StringBuilder b = new StringBuilder(input.toString(2)).reverse();
+        b.setLength(b.length() - 1); // remove marker.
+        return a.or(new BigInteger(b.toString(), 2)
+                .xor(BigInteger.ZERO.setBit(b.length()).subtract(BigInteger.ONE)));
+    }
+
+    private BigInteger computeChecksum(BigInteger input) {
+        BigInteger evenBitMask = BigInteger.ZERO.setBit(input.bitLength() - 1)
+                .divide(BigInteger.valueOf(3)); // Every even bit set.
+        // Bits are 1 if pairs are same, 0 otherwise.
+        BigInteger x = input.xor(input.shiftRight(1)).and(evenBitMask).xor(evenBitMask);
+        // Convert 4-ary with [0, 1] to binary, and append marker.
+        return new BigInteger(x.toString(4), 2).setBit((input.bitLength() - 1) / 2);
+    }
+
+    @Override
+    public String compute(BufferedReader input) {
+        BigInteger initialState = BigInteger.valueOf(SEED | Integer.highestOneBit(SEED) << 1);
+
+        String checksum = Stream.iterate(initialState, this::generateDragonCurve)
+                .filter(state -> state.bitLength() > LIMIT)
+                .map(state -> state.shiftRight(state.bitLength() - 1 - LIMIT)) // Eliminate all but the first LIMIT bits.
+                .findFirst()
+                .flatMap(state -> Stream.iterate(computeChecksum(state.setBit(LIMIT)), this::computeChecksum)
+                        .filter(check -> check.bitLength() % 2 == 0) // If there is an odd number of bits plus marker.
+                        .map(check -> check.toString(2).substring(1)) // Make binary String without marker.
+                        .findFirst())
+                .orElse("");
+
+        return "The checksum for the data is " + checksum;
+    }
+}
