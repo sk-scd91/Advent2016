7815bb6292bbe49780d7fff1759b916782c63009
sk-scd91 <scdeneen@gmail.com>
2016-12-18 15:14:27 -0500

    Completed the Solution for Day 18 Part 1.

---
+++ advents/AdventDay18Part1.java
@@ -0,0 +1,59 @@
+package com.skscd91.advent2016.advents;
+
+import com.skscd91.advent2016.utils.IOUtils;
+
+import java.io.BufferedReader;
+import java.util.BitSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by Sean Deneen on 12/18/16.
+ * Find the number of tiles in a room that are not traps, using a 1D cellular automata.
+ */
+public class AdventDay18Part1 implements Advent {
+
+    private static final char SAFE_TILE = '.';
+    private static final char TRAP_TILE = '^';
+
+    private BitSet rule;
+
+    private BitSet getTrapRule() {
+        BitSet rule = new BitSet();
+        rule.set(0b110); // left and center.
+        rule.set(0b011); // center and right.
+        rule.set(0b100); // left only.
+        rule.set(0b001); // right only.
+        return rule; // Rule 90.
+    }
+
+    private boolean isTrap(String line, int index) {
+        return (index >= 0 && index < line.length() // Out of bounds tiles are treated as safe.
+                && line.charAt(index) == TRAP_TILE);
+    }
+
+    private String generateNextLine(String lastLine) {
+        return IntStream.range(0, lastLine.length())
+                .map(i -> IntStream.rangeClosed(i - 1, i + 1)
+                        .filter(iCenter -> isTrap(lastLine, iCenter))
+                        .reduce(0, (n ,shift) -> n | 1 << (1 - (shift - i))))
+                .mapToObj(n -> String.valueOf(rule.get(n) ? TRAP_TILE : SAFE_TILE))
+                .collect(Collectors.joining());
+    }
+
+    @Override
+    public String compute(BufferedReader input) {
+        rule = getTrapRule();
+
+        long count = Stream.iterate(IOUtils.readLineSilently(input),
+                this::generateNextLine)
+                .limit(40L)
+                .peek(System.out::println)
+                .flatMapToInt(String::chars)
+                .filter(c -> c == SAFE_TILE)
+                .count();
+
+        return "The number of safe tiles in the room is " + count;
+    }
+}
